738d2b6fea56eb24390f392e4d3bcf37c39fb691
David Ullrich <david.ullrich@ullrich-software.at>
2023-01-25 21:56:31 +0100

    Add test project

---
+++ ClassLibrary.Tests/Component1Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using RazorClassLibrary1.Components;
+using TestContext = Bunit.TestContext;
+
+namespace ClassLibrary.Tests
+{
+    public class Component1Tests
+    {
+        [Test]
+        public void ShouldRenderComponent()
+        {
+            var ctx = new TestContext();
+
+            var _ = ctx.RenderComponent<Component1>();
+        }
+
+        [Test]
+        public void ShouldContainOsVersionAndUserName()
+        {
+            var ctx = new TestContext();
+
+            var component = ctx.RenderComponent<Component1>();
+
+            Assert.True(component.Markup.Contains("OS Version"));
+            Assert.True(component.Markup.Contains("User name"));
+        }
+    }
+}
